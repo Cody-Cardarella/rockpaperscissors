5a48bf9ee9f4a4e3db464f742ff773e6ef844aae
Cody <115433936+Cody-Cardarella@users.noreply.github.com>
2022-11-07 18:05:51 -0700

    Clean up code

--- javascript.js
+++ javascript.js
@@ -8,51 +8,52 @@ let playerWin = 0;
 
 // Function to generate a random option for computer
 function getComputerChoice() {
-
     let number = Math.floor(Math.random() * 3);
     let computerOption = options[number];
     return computerOption;
-    
 };
 
 // Function to play a round and determine winner
 function playRound(playerChoice, computerChoice){
-
     if (playerChoice == computerChoice){
         draw++;
         console.log('It\'s a draw');
-    } else if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS' || playerChoice == 'SCISSORS' && computerChoice == 'PAPER' || playerChoice == 'PAPER' && computerChoice == 'ROCK'){
+
+    } else if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS' || 
+              playerChoice == 'SCISSORS' && computerChoice == 'PAPER' || 
+              playerChoice == 'PAPER' && computerChoice == 'ROCK'){
         playerWin++;
         console.log('Player won this round');
-    } else if (playerChoice == 'ROCK' && computerChoice == 'PAPER' || playerChoice == 'PAPER' && computerChoice == 'SCISSORS' || playerChoice == 'SCISSORS' && computerChoice == 'ROCK' ) {
+
+    } else if (playerChoice == 'ROCK' && computerChoice == 'PAPER' || 
+              playerChoice == 'PAPER' && computerChoice == 'SCISSORS' || 
+              playerChoice == 'SCISSORS' && computerChoice == 'ROCK' ) {
         pcWin++;
         console.log('Computer has won this round');
+
     } else {
+
         console.log('You must\'ve made a typo.')
+
     };
-    
- };
+};
 
  // Looping the game 5 times
 function game() {
-
     for (let i = 1; i <=5; i++) {
-    theOne = prompt('Pick Rock, Paper, or Scissors!').toUpperCase(); // Assign user selection from prompt
+    theOne = prompt('Pick Rock, Paper, or Scissors!').toUpperCase();
     playRound(theOne, computerSelection);
     };
-
 };
 
 // Deciding the winner
 function runGame (){
-
     game();
     if (playerWin > pcWin){
         console.log('Player has won')
     } else {
         console.log('PC has won')
     };
-
 };
 
 runGame();
