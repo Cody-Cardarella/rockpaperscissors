9a8700a379cdad076e0fd604bc4d4d0c3a6152ef
Cody <115433936+Cody-Cardarella@users.noreply.github.com>
2022-11-09 16:32:36 -0700

    Change game to accept button input instead of prompt

--- javascript.js
+++ javascript.js
@@ -1,59 +1,68 @@
 //Rock Paper Scissors
-const options = ["Rock", "Paper","Scissors"]; // Define the game options
-let computerSelection = getComputerChoice().toUpperCase(); // Assign the computer selection from function
-let theOne;
-let draw = 0;
-let pcWin = 0;
-let playerWin = 0;
+const options = ["Rock", "Paper","Scissors"];
+let btnClick = document.querySelectorAll('button')
+let results = document.querySelector('.winner')
+let playercounter = document.querySelector('.pcounter')
+let computercounter = document.querySelector('.ccounter')
+let announce = document.querySelector('.announce')
+let pCounter = 0;
+let cCounter = 0;
 
 // Function to generate a random option for computer
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     let computerOption = options[number];
-    return computerOption;
+    return computerOption.toUpperCase();
 };
 
-// Function to play a round and determine winner
-function playRound(playerChoice, computerChoice){
-    if (playerChoice == computerChoice){
-        draw++;
-        console.log('It\'s a draw');
+// Grab player choice
+function playerChoiceFn() {
+    btnClick.forEach((button) => {
+        button.addEventListener('click', () => {
+            let theChoice = button.className.toUpperCase();
+            playRound(theChoice, getComputerChoice());
+                });
+            });
+    }
 
-    } else if (playerChoice == 'ROCK' && computerChoice == 'SCISSORS' || 
-              playerChoice == 'SCISSORS' && computerChoice == 'PAPER' || 
-              playerChoice == 'PAPER' && computerChoice == 'ROCK'){
-        playerWin++;
-        console.log('Player won this round');
+// FPlay round and log the winner
+function playRound(pChoice, cChoice){
 
-    } else if (playerChoice == 'ROCK' && computerChoice == 'PAPER' || 
-              playerChoice == 'PAPER' && computerChoice == 'SCISSORS' || 
-              playerChoice == 'SCISSORS' && computerChoice == 'ROCK' ) {
-        pcWin++;
-        console.log('Computer has won this round');
+    computercounter.textContent = cCounter;
+    playercounter.textContent = pCounter;
+
+    if (pChoice == cChoice){
+        results.textContent = 'It\'s a draw';
+
+    } else if (pChoice == 'ROCK' && cChoice == 'SCISSORS' || 
+              pChoice == 'SCISSORS' && cChoice == 'PAPER' || 
+              pChoice == 'PAPER' && cChoice == 'ROCK'){
+              results.textContent = 'Player won this round';
+              
+              pCounter++;
+              
+
+    } else if (pChoice == 'ROCK' && cChoice == 'PAPER' || 
+              pChoice == 'PAPER' && cChoice == 'SCISSORS' || 
+              pChoice == 'SCISSORS' && cChoice == 'ROCK' ) {
+              results.textContent = 'Computer has won this round';
+              
+              cCounter++;
 
     } else {
 
-        console.log('You must\'ve made a typo.')
+        results.textContent = ('You must\'ve made a typo.')
 
+    };
+    if (pCounter == 6){
+        announce.textContent = "Player Won";
+    } if (cCounter == 6){
+        announce.textContent = "Computer Won";
     };
 };
 
- // Looping the game 5 times
-function game() {
-    for (let i = 1; i <=5; i++) {
-    theOne = prompt('Pick Rock, Paper, or Scissors!').toUpperCase();
-    playRound(theOne, computerSelection);
-    };
-};
 
-// Deciding the winner
-function runGame (){
-    game();
-    if (playerWin > pcWin){
-        console.log('Player has won')
-    } else {
-        console.log('PC has won')
-    };
-};
 
-runGame();
+playerChoiceFn();
+
+
